abe034503663e3d41a31f13a1185a6b71c6eb1a8
Stormn13 <akshat12185@gmail.com>
2026-05-14 16:02:29 +0530

    added borders of the camera

--- scripts/random_bridge.gd
+++ scripts/random_bridge.gd
@@ -2,6 +2,8 @@ extends Node3D
 var random_value: float
 @onready var glitch_rect := $CanvasLayer/ColorRect
 @onready var shader_mat := glitch_rect.material as ShaderMaterial
+@export_range(0.0, 1) var intensity: float = 0.99
 func _process(delta: float) -> void:
 	random_value = randf()
 	shader_mat.set_shader_parameter("random_value" , random_value)
+	shader_mat.set_shader_parameter("intensity", intensity)
