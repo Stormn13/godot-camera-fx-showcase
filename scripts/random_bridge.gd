93193420ac9ef45b3f4827b31411a0ad629732aa
Stormn13 <akshat12185@gmail.com>
2026-05-15 23:12:41 +0530

    timer added

--- scripts/random_bridge.gd
+++ scripts/random_bridge.gd
@@ -2,7 +2,7 @@ extends Node3D
 var random_value: float
 @onready var glitch_rect := $CanvasLayer/ColorRect
 @onready var shader_mat := glitch_rect.material as ShaderMaterial
-@export_range(0.0, 1) var intensity: float = 0.99
+@export_range(0.0, 1) var intensity: float = 0.95
 func _process(delta: float) -> void:
 	random_value = randf()
 	shader_mat.set_shader_parameter("random_value" , random_value)
